58625b269065707934bf2a961131fbdf15a14849
Furkan Cırıt <furkancrt96@gmail.com>
2024-11-05 22:41:35 +0300

    waitForKafka

---
+++ Kafka/waitForKafka.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# wait_for_kafka.sh
+
+KAFKA_HOST="broker"
+KAFKA_PORT="9092"
+
+# Wait for Kafka to be ready
+while ! nc -z $KAFKA_HOST $KAFKA_PORT; do
+  echo "Waiting for Kafka broker to be ready..."
+  sleep 5
+done
+
+echo "Kafka broker is ready. Creating topic..."
+# Place your topic creation command here
+kafka-topics --bootstrap-server $KAFKA_HOST:$KAFKA_PORT --create --topic ${TOPIC_NAME} --partitions ${PARTITION_COUNT} --replication-factor ${REPLICA_COUNT}
